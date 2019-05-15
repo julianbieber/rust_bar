e86b6d06f1a7158f121ffd3cdec8f0e06926ba43
Julian Bieber <j.bieber@patagona.de>
2019-05-15 17:53:13 +0200

    added idle time to bar

--- src/config.rs
+++ src/config.rs
@@ -1,6 +1,5 @@
-
-use std::path::{PathBuf, Path};
 use dirs::home_dir;
+use std::path::{Path, PathBuf};
 
 pub fn base_path() -> PathBuf {
     home_dir()
--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,30 @@ mod config;
 
 use chrono::Local;
 
+use std::error::Error;
+use std::fs::{read_dir, OpenOptions};
+use std::io::{BufRead, BufReader};
 use std::process::Command;
+use std::str;
 use std::thread::sleep;
 use std::time::Duration;
-use std::fs::{read_dir, OpenOptions};
-use std::io::{BufReader, BufRead};
-use std::error::Error;
 
 fn time_string() -> String {
     let now = Local::now();
     format!("{}", now.format("(%H:%M) (%d-%m-%Y)"))
+}
+
+fn idle_time() -> String {
+    let mut string = str::from_utf8(
+        &Command::new("getIdle")
+            .output()
+            .expect("Failed to run getIdle")
+            .stdout,
+    )
+    .expect("Could not parse the output of getIdle")
+    .to_string();
+    let _ = string.pop();
+    string
 }
 
 fn get_current_tasks() -> std::result::Result<String, Box<dyn Error>> {
@@ -22,7 +36,7 @@ fn get_current_tasks() -> std::result::Result<String, Box<dyn Error>> {
         let file = file_result?;
         if file.path().is_file() {
             let reader = BufReader::new(OpenOptions::new().read(true).open(file.path())?);
-            if reader.lines().count() % 2 == 1{
+            if reader.lines().count() % 2 == 1 {
                 tasks.push(file.file_name().into_string().expect(""))
             }
         }
@@ -32,7 +46,16 @@ fn get_current_tasks() -> std::result::Result<String, Box<dyn Error>> {
 
 fn main() -> std::result::Result<(), Box<dyn Error>> {
     loop {
-        Command::new("xsetroot").arg("-name").arg(format!("{} {}", get_current_tasks()?, time_string())).output().expect("");
+        Command::new("xsetroot")
+            .arg("-name")
+            .arg(format!(
+                "{} | {} {}",
+                idle_time(),
+                get_current_tasks()?,
+                time_string()
+            ))
+            .output()
+            .expect("");
         sleep(Duration::new(1, 0));
     }
 }
