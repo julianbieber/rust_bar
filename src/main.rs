7c35b9f3e3770744639207b6abcd4880cdc16f62
Julian Bieber <j.bieber@patagona.de>
2019-05-31 16:33:53 +0200

    added battery output

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 extern crate chrono;
-
+extern crate battery;
 mod config;
 
 use chrono::Local;
@@ -19,15 +19,15 @@ fn time_string() -> String {
 
 fn idle_time() -> String {
     let mut string = str::from_utf8(
-        &Command::new("getIdle")
+        &Command::new("xssstate").arg("-i")
             .output()
-            .expect("Failed to run getIdle")
+            .expect("Failed to run xssstste")
             .stdout,
     )
     .expect("Could not parse the output of getIdle")
     .to_string();
     let _ = string.pop();
-    string
+    format!("{}", string.parse::<u32>().expect("Failed to parse idle state") / 60000)
 }
 
 fn get_current_tasks() -> std::result::Result<String, Box<dyn Error>> {
@@ -41,15 +41,32 @@ fn get_current_tasks() -> std::result::Result<String, Box<dyn Error>> {
             }
         }
     }
-    Ok(tasks.join(" | "))
+    let mut tasks_string = tasks.join(" | ");
+    if !tasks.is_empty(){
+        tasks_string.push(' ');
+    }
+    Ok(tasks_string)
+}
+
+fn get_battery_string(battery_manager: &battery::Manager) -> Option<String> {
+    battery_manager.batteries().ok().and_then(|mut batteries| {
+        batteries.next().and_then(|battery_opt| {
+            match battery_opt {
+                Ok(battery) => Some(format!("{:.2}", f32::from(battery.state_of_charge()))),
+                _ => None
+            }
+        })
+    })
 }
 
 fn main() -> std::result::Result<(), Box<dyn Error>> {
+    let battery_manager = battery::Manager::new()?;
     loop {
         Command::new("xsetroot")
             .arg("-name")
             .arg(format!(
-                "{} | {} {}",
+                " {} | {} | {}{}",
+                get_battery_string(&battery_manager).unwrap_or_else(||{String::new()}),
                 idle_time(),
                 get_current_tasks()?,
                 time_string()
